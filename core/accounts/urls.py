96cd5a0cbef47717688f76ccaa9eade2930edaa8
reshadMajumder <reshad22205341197@diu.edu.bd>
2025-11-04 15:33:40 +0600

    added token refresh

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from .views import RegisterView,ProfileView,LoginView,LogoutView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-
-
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 ]
